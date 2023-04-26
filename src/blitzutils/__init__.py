879ff113ed10ae0b729cafb9207acfa17b30bacf
Jylpah <jylpah@gmail.com>
2023-04-26 11:01:08 +0300

    Make module re-importable

--- src/blitzutils/__init__.py
+++ src/blitzutils/__init__.py
@@ -1,16 +1,33 @@
-from .wg 			import WGApi, WoTBlitzMaps
-from .wotinspector 	import WoTBlitzReplayJSON, WoTinspector
-from .region 		import Region
-from .release 		import WGBlitzRelease
-from .account 		import Account, WGAccountInfo
-from .tank 			import EnumNation, EnumVehicleTier, EnumVehicleTypeInt, \
-    						EnumVehicleTypeStr, WGTank, Tank
-from .wg_api 		import WGAccountInfo, WGApiError, WGApiWoTBlitz,\
-    						WGApiTankopedia,  WGApiWoTBlitzAccountInfo, \
-                            WGApiWoTBlitzPlayerAchievements, WGApiWoTBlitzTankStats, \
-                            WGPlayerAchievements, WGPlayerAchievementsMain, \
-                            WGPlayerAchievementsMaxSeries, \
-                            WGTankStat, WGTankStatAll, WoTBlitzTankString
+from .wg 			import WGApi as WGApi,\
+     					  WoTBlitzMaps as WoTBlitzMaps
+from .wotinspector 	import WoTinspector as WoTinspector
+from .replay 		import WoTBlitzReplayJSON as WoTBlitzReplayJSON,\
+     						WoTBlitzReplayData as WoTBlitzReplayData, \
+                            WoTBlitzReplaySummary as WoTBlitzReplaySummary, \
+                            WoTBlitzReplayDetail as WoTBlitzReplayDetail, \
+                            WoTBlitzReplayAchievement as WoTBlitzReplayAchievement
+from .region 		import Region as Region
+from .release 		import WGBlitzRelease as WGBlitzRelease
+from .account 		import Account as Account
+from .tank 			import EnumNation as EnumNation, \
+    						EnumVehicleTier as EnumVehicleTier, \
+                            EnumVehicleTypeInt as EnumVehicleTypeInt, \
+    						EnumVehicleTypeStr as EnumVehicleTypeStr, \
+                            Tank as Tank, \
+                            WGTank as WGTank
+from .wg_api 		import WGAccountInfo as WGAccountInfo, \
+     					 	WGApiError as WGApiError, \
+                            WGApiWoTBlitz as WGApiWoTBlitz, \
+    						WGApiTankopedia as WGApiTankopedia, \
+                            WGApiWoTBlitzAccountInfo as WGApiWoTBlitzAccountInfo, \
+                            WGApiWoTBlitzPlayerAchievements as WGApiWoTBlitzPlayerAchievements, \
+                            WGApiWoTBlitzTankStats as WGApiWoTBlitzTankStats, \
+                            WGPlayerAchievements as WGPlayerAchievements, \
+                            WGPlayerAchievementsMain as WGPlayerAchievementsMain, \
+                            WGPlayerAchievementsMaxSeries as WGPlayerAchievementsMaxSeries, \
+                            WGTankStat as WGTankStat, \
+                            WGTankStatAll as WGTankStatAll, \
+                            WoTBlitzTankString as WoTBlitzTankString
 
 __all__ = [ 'wg', 
            	'wg_api',
