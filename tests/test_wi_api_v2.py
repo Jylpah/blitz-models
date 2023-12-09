d5044b114289e9b0989d14dd1c97ddcfc639fc91
Jylpah <jylpah@gmail.com>
2023-12-09 17:22:52 +0200

    add tests for WI API v2 models: Replay, PlayerData

---
+++ tests/test_wi_api_v2.py
@@ -0,0 +1,55 @@
+import sys
+import pytest  # type: ignore
+from os.path import dirname, realpath, join as pjoin, basename
+from pathlib import Path
+import logging
+import json
+
+logger = logging.getLogger()
+error = logger.error
+message = logger.warning
+verbose = logger.info
+debug = logger.debug
+
+sys.path.insert(0, str(Path(__file__).parent.parent.resolve() / "src"))
+
+from blitzutils import (
+    Account,
+    Region,
+    WGApi,
+    AccountInfo,
+    PlayerAchievementsMaxSeries,
+    TankStat,
+    WGApiWoTBlitzTankopedia,
+    Tank,
+    WGApiTankString,
+)
+
+from blitzutils.wi_apiv2 import Replay, PlayerData
+
+########################################################
+#
+# Test Plan
+#
+########################################################
+
+# 1) test models
+
+########################################################
+#
+# Fixtures
+#
+########################################################
+
+FIXTURE_DIR = Path(__file__).parent
+
+
+@pytest.mark.asyncio
+async def test_1_models() -> None:
+    """test for models"""
+    assert (
+        r := Replay.example_instance()
+    ) is not None, "could not parse the Replay example instance"
+    assert (
+        pd := PlayerData.example_instance()
+    ) is not None, "could not parse the PlayerData example instance"
